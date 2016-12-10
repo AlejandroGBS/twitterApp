e47fae445f328bb35c98c19b80c68504d773238e
xander <xander@Debian-Server>
2016-12-10 21:24:20 +0100

    modificado kafka start

--- twitterApp/src/main/java/com/master2016/KafkaStart.java
+++ twitterApp/src/main/java/com/master2016/KafkaStart.java
@@ -3,15 +3,14 @@ package com.master2016;
 public class KafkaStart {
 
 	KafkaTwitterProducer kafkaTwitterProducer = new KafkaTwitterProducer();
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		 KafkaStart kafkaStart = new KafkaStart();
-		
-		 
-		if(args.length == 7){
-			
+		KafkaStart kafkaStart = new KafkaStart();
+
+		if (args.length == 7) {
+
 			Keys.mode = args[0];
 			Keys.apiKey = args[1];
 			Keys.apiSecret = args[2];
@@ -19,23 +18,18 @@ public class KafkaStart {
 			Keys.tokenSecret = args[4];
 			Keys.brokerUrl = args[5];
 			Keys.fileName = args[6];
-			
+
 		}
-		
-		for(int i = 0; i< args.length;i++){
+
+		for (int i = 0; i < args.length; i++) {
 			System.out.println(args[i]);
 		}
-		
 
-			try{
-				kafkaStart.kafkaTwitterProducer.fromTwitter();
-			}catch(Exception ex){
-				System.out.println(ex);
-			}
-
-		
-		
-
+		try {
+			kafkaStart.kafkaTwitterProducer.fromTwitter();
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
 	}
 
 }
